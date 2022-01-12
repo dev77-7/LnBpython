3e5b543d61b73699fc9d710a4caefa00fae741d5
dev77-7 <63906651+dev77-7@users.noreply.github.com>
2022-01-12 18:29:39 +0530

    Fixed FleNotFoundError with os module

--- Assignments 3/lnbsearch.py
+++ Assignments 3/lnbsearch.py
@@ -1,5 +1,7 @@
+import os
+path = r'C:\Users\shubh\Desktop\LnB_assignments\Assignments 3\sample.txt'
 a=input("Enter String to Search:- ")
-sample=open("sample.txt","r")
+sample=open(path,"r")
 current=0
 stop=0
 for line in sample:
