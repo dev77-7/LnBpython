0c59592fee870f96309ef82201eaee30987f76ce
dev77-7 <63906651+dev77-7@users.noreply.github.com>
2022-01-15 16:17:21 +0530

    Assignment 3 Question 2

---
+++ Assignments 3/lnb_brain.py
@@ -0,0 +1,25 @@
+from cgitb import text
+import os
+from re import T
+from gtts import gTTS
+from playsound import playsound
+for i in range(1,4):
+    a=input("Enter the name of the directory:- ")
+    parent=r"C:\Users\shubh\Desktop\LnB_assignments\Assignments 3"
+    mode=0o666
+    path=os.path.join(parent,a)
+    isFile=os.path.isfile(path)
+    try:
+        os.makedirs(path,mode)
+        b="Dimame created"
+        language='en'
+        final=gTTS(text=b,lang=language,slow=True)
+        final.save("out.mp3")
+        playsound("out.mp3")
+    except FileExistsError:
+        b="dimame already exists"
+        language='en'
+        final=gTTS(text=b,lang=language,slow=True)
+        final.save("out.mp3")
+        playsound("out.mp3")
+    
